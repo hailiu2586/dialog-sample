06d617436662819410f067bf49d9f0bd8f762817
Hai Liu <hailiu@gmail.com>
2019-06-19 14:54:14 -0700

    pull all branches

--- fetchAll.cmd
+++ fetchAll.cmd
@@ -33,18 +33,41 @@ git fetch --all
 
 rem create work tree for  all branches
 FOR /F "tokens=* usebackq" %%b in (`git branch -r`) do (
-    call :add_worktree %%b
+    call :prep_worktree %%b
 )
 
 goto :eof
 
+:prep_worktree
+set bn=%1
+set sbn=%bn:~4%
+IF EXIST %DEPLOYMENT_TARGET%/%sbn% (
+    call :pull_worktree %bn%
+) else (
+    call :add_worktree %bn%
+)
+
+exit /b
 
 :add_worktree
 set bn=%1
 set sbn=%bn:~4%
+if %sbn%==master exit /b
 echo add worktree for branch %sbn%
-git worktree add %DEPLOYMENT_TARGET%/%bn:~4% %bn:~4%
+git worktree add %DEPLOYMENT_TARGET%/%sbn% %sbn%
+
 exit /b
+
+:pull_worktree
+set bn=%1
+set sbn=%bn:~4%
+echo pull worktree branch %sbn%
+pushd %DEPLOYMENT_TARGET%\%sbn:/=\%
+git pull --rebase
+popd
+
+exit /b
+
 
 :eof
 endlocal
