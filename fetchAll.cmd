8eaefa042a2c35520150596ac3e4465d60a24ae8
Hai Liu <hailiu@gmail.com>
2019-06-19 14:16:33 -0700

    push fetchAll

--- fetchAll.cmd
+++ fetchAll.cmd
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-setlocal
-
-rem get the remote origin
-FOR /F "tokens=* usebackq" %k in (`git remote get-url origin`) do (
-    set REMOTE_GIT=%~k
-=======
 @echo on
 setlocal
 
@@ -28,23 +21,12 @@ if not "x%GIT_FETCH:+refs/heads/*:refs/remotes/git/*=%x"=="x%GIT_FETCH%x" goto :
 rem get the remote origin
 FOR /F "tokens=* usebackq" %%k in (`git remote get-url origin`) do (
     set REMOTE_GIT=%%~k
->>>>>>> fetch all branches as part of deploy
 )
 
 rem config remote to fetch all branches
 git remote add git %REMOTE_GIT%
 git remote remove origin
 
-<<<<<<< HEAD
-rem fetch all branches
-git fetch --all
-
-rem list all remote branches
-git branch -r --format=^%(refname:lstrip=3) > branches.txt
-FOR /F %b in (branches.txt) do (
-    git --work-tree=\scrach\dump\fetchAll\%b checkout %b
-)
-=======
 :worktree
 rem fetch all branches
 git fetch --all
@@ -65,5 +47,4 @@ git worktree add %DEPLOYMENT_TARGET%/%bn:~4% %bn:~4%
 exit /b
 
 :eof
->>>>>>> fetch all branches as part of deploy
 endlocal
